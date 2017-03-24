edf97ff8d912f9d37778660e788f87f8604456fd
nielsv <niels.vandekeybus@tenforce.com>
2017-03-24 10:51:49 +0100

    Fixed: move flags to /data
    The file flags were stored in the container directly: if you mount the
    database directory from your host, the file flags are only stored in the
    container.
    
    The problem with this approach is that the container can be out-of-sync
    with the actual database state. For instance, if you re-create the
    container, the script may load the data in the toLoad directory
    despite they have been loaded already because the flags have been
    removed with the containers.
    
    The inverse problem may occurs too: if you stop the container, delete
    the database directory and restart the container, then the data in
    toLoad directory will not be loaded because it considers they have been
    loaded already because the flags are still there.

--- virtuoso.sh
+++ virtuoso.sh
@@ -11,7 +11,7 @@ fi
 chmod +x /clean-logs.sh
 mv /clean-logs.sh . 2>/dev/null
 
-if [ ! -f "/.config_set" ];
+if [ ! -f ".config_set" ];
 then
   echo "Converting environment variables to ini file"
   printenv | grep -P "^VIRT_" | while read setting
@@ -22,21 +22,21 @@ then
     echo "Registering $section[$key] to be $value"
     crudini --set virtuoso.ini $section $key $value
   done
-  touch /.config_set
+  touch .config_set
   echo "Finished converting environment variables to ini file"
 fi
 
-if [ ! -f "/.dba_pwd_set" ];
+if [ ! -f ".dba_pwd_set" ];
 then
   touch /sql-query.sql
   if [ "$DBA_PASSWORD" ]; then echo "user_set_password('dba', '$DBA_PASSWORD');" >> /sql-query.sql ; fi
   if [ "$SPARQL_UPDATE" = "true" ]; then echo "GRANT SPARQL_UPDATE to \"SPARQL\";" >> /sql-query.sql ; fi
   virtuoso-t +wait && isql-v -U dba -P dba < /dump_nquads_procedure.sql && isql-v -U dba -P dba < /sql-query.sql
   kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
-  touch /.dba_pwd_set
+  touch .dba_pwd_set
 fi
 
-if [ ! -f "/.data_loaded" ] && [ -d "toLoad" ] ;
+if [ ! -f ".data_loaded" -a -d "toLoad" ] ;
 then
     echo "Start data loading from toLoad folder"
     pwd="dba"
@@ -51,7 +51,7 @@ then
     echo "$(cat /load_data.sql)"
     virtuoso-t +wait && isql-v -U dba -P "$pwd" < /load_data.sql
     kill $(ps aux | grep '[v]irtuoso-t' | awk '{print $2}')
-    touch /.data_loaded
+    touch .data_loaded
 fi
 
 virtuoso-t +wait +foreground
